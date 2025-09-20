1bae779ff573b2f5df889e237d2e77934b62a7a1
Gabriel <gabriel.wallace.3998@outlook.com>
2025-09-20 19:55:41 -0300

    Atualizando comentários

--- Gráfico da onda Senoidal.py
+++ Gráfico da onda Senoidal.py
@@ -1,9 +1,19 @@
+# Importa as bibliotecas necessárias
+import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Cria um vetor com 100 valores igualmente espaçados de 0 a 10
 x = np.linspace(0, 10, 100)
+
+# Calcula o seno para cada valor do vetor x, criando o vetor y
 y = np.sin(x)
 
+# Cria a figura e o eixo do gráfico
 fig, ax = plt.subplots()
+
+# Plota o vetor y em função do vetor x
 ax.plot(x, y)
+
+# Exibe o gráfico na tela
 plt.show()
